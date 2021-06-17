c096b8578a0572ab28401cca7e9ded52be5e310e
785146 <785146@SAIT222460IT.ACDM.DS.SAIT.CA>
2021-06-17 01:32:21 -0600

    Added doGet() method Added a fully functioning doGet() method in the login servlet Fixed some minor errors

--- src/java/servlets/LoginServlet.java
+++ src/java/servlets/LoginServlet.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import models.User;
+import services.AccountService;
 
 public class LoginServlet extends HttpServlet {
 
@@ -20,20 +22,43 @@ public class LoginServlet extends HttpServlet {
         
         if (logout != null) {
             session.invalidate();
-            request.setAttribute("message", "You have been logged out");
+            request.setAttribute("message", "You have successfully logged out");
             getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
             
         } else if (username != null) {
             response.sendRedirect("home");
             
         } else {
-            getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+            getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);  
         }       
     }
     
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-               
+        
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        
+        if (username.equals("") || password.equals("")) {
+            request.setAttribute("message", "Please enter a username and password to login");
+            getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+            
+        } else {
+            AccountService service = new AccountService();
+            User user = service.login(username, password);
+            
+            if (user != null) {
+                HttpSession session = request.getSession();
+                session.setAttribute("username", username);
+                response.sendRedirect("home");
+            
+            } else {
+                request.setAttribute("username", username);
+                request.setAttribute("password", password);
+                request.setAttribute("message", "Username or password is incorrect");
+                getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+            }
+        }       
     }
 }
